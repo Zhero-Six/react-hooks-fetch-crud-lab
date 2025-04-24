ed71b3651fb5553dca24600ad9f754d896f81157
Zhero-Six <vmochoge@gmail.com>
2025-04-25 01:36:28 +0300

    updates

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  function handleDeleteQuestion(id) {
+    setQuestions(questions.filter((question) => question.id !== id));
+  }
+
+  function handleUpdateQuestion(updatedQuestion) {
+    setQuestions(
+      questions.map((question) =>
+        question.id === updatedQuestion.id ? updatedQuestion : question
+      )
+    );
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDeleteQuestion={handleDeleteQuestion}
+            onUpdateQuestion={handleUpdateQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
